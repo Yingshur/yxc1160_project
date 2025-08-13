ee43727998e081b48cf26712bdd2bdcc36ac9b09
Yingshur <chenyingshu1234@gmail.com>
2025-08-13 21:57:30 +0100

    new modifications to the website

--- config.py
+++ config.py
@@ -8,7 +8,10 @@ class Config:
 
     UPLOAD_FOLDER = os.path.join(basedir, 'app', 'data', 'uploads')
     MAX_CONTENT_LENGTH = 1 * 1024 * 1024
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user_flask:Chenxh1031%21@localhost/roman_history'
+    SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://avnadmin:AVNS_ZtThNkudDKdZCTn-5Q5"
+                               "@mysql-1ade3b12-chenyingshu1234-4999.e.aivencloud.com:18443/defaultdb"
+                               "?ssl_ca="+ os.path.join(basedir, "app", "static", "ca.pem"))
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
 
