af37bd7ef975bdcbfdf2e71e92805ba4cbc33503
Yingshur <chenyingshu1234@gmail.com>
2025-08-14 01:00:15 +0100

    new modifications to the website

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 if __name__ == '__main__':
     with app.app_context():
         schedule_start()
-        app.run(port=5812)
+        app.run(port=5813)
 
 
     #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7899)))
